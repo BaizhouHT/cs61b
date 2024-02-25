e03cdf3e8961bd1b6c0bf1fa775d7df82da5192e
BaizhouTao <harrisontweaver@gmail.com>
2024-02-25 14:31:26 +0800

    1. LLD: solve prev commit problem by initialize the sentinel with null prev and next; 2. new ArrayDeque.java

---
+++ proj1/deque/ArrayDeque.java
@@ -0,0 +1,10 @@
+package deque;
+
+/**
+ * @Description: data structure arrayList
+ * @Author: Harrison
+ * @Date: 2/25/2024 2:28 PM
+ * @Version: 1.0
+ */
+public class ArrayDeque {
+}
--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -33,39 +33,38 @@ public class LinkedListDeque<T> {
      * Constructor of deque, initialize an empty deque
      */
     public LinkedListDeque() {
-        sentinelOrigin = new Node(999, sentinelOrigin,sentinelOrigin); //TODO: fix the null mis
-//        currNode = sentinelOrigin;
+        sentinelOrigin = new Node(999, null,null); //TODO: fix the null mis
         size = 0;
     }
 
     public void addFirst(T item) {
-        Node new1stNode = new Node(item, null, null);
-        // update in
-        new1stNode.prev = sentinelOrigin;
-        new1stNode.next = sentinelOrigin.next;
-        System.out.println(sentinelOrigin.next);
-        System.out.println(new1stNode.next);
-        // update in.next(in+1)
-        new1stNode.next.prev = new1stNode;
-        // update sentinel
-        sentinelOrigin.next = new1stNode;
-        if (sentinelOrigin.prev == sentinelOrigin) {
-            sentinelOrigin.prev = new1stNode;
+        Node temp = new Node(item, null, null);
+        if (size == 0) {
+            sentinelOrigin.next = temp;
+            sentinelOrigin.prev = temp;
+            temp.prev = sentinelOrigin;
+            temp.next = sentinelOrigin;
+        } else {
+            temp.next = sentinelOrigin.next;
+            temp.prev = sentinelOrigin;
+            sentinelOrigin.next.prev = temp;
+            sentinelOrigin.next = temp;
         }
         size += 1;
     }
 
     public void addLast(T item) {
-        Node newLastNode = new Node(item, null, null);
-        // update in
-        newLastNode.prev = sentinelOrigin.prev;
-        newLastNode.next = sentinelOrigin;
-        // update in.prev(in-1)
-        newLastNode.prev.next = newLastNode;
-        // update sentinel
-        sentinelOrigin.prev = newLastNode;
-        if (sentinelOrigin.next == sentinelOrigin) {
-            sentinelOrigin.next = newLastNode;
+        Node temp = new Node(item, null, null);
+        if (size == 0) {
+            sentinelOrigin.next = temp;
+            sentinelOrigin.prev = temp;
+            temp.prev = sentinelOrigin;
+            temp.next = sentinelOrigin;
+        } else {
+            temp.prev = sentinelOrigin.prev;
+            temp.next = sentinelOrigin;
+            sentinelOrigin.prev.next = temp;
+            sentinelOrigin.prev = temp;
         }
         size += 1;
     }
@@ -94,45 +93,34 @@ public class LinkedListDeque<T> {
     }
 
     public T removeFirst() {
-        if (sentinelOrigin.next == sentinelOrigin) {
-            return null;
+        T removeEle = null;
+        if (size == 0) {
+            return removeEle;
         }
-        T removeEle = (T) sentinelOrigin.next.item;
-        if (sentinelOrigin.next.next != null) {// after remove size > 0
-            sentinelOrigin.next.next.prev = sentinelOrigin;
-            sentinelOrigin.next = sentinelOrigin.next.next;
-        } else {// after remove size = 0
-            sentinelOrigin.next = sentinelOrigin;
-            sentinelOrigin.prev = sentinelOrigin;
-        }
+        removeEle = (T) sentinelOrigin.next.item;
+        sentinelOrigin.next.next.prev = sentinelOrigin;
+        sentinelOrigin.next = sentinelOrigin.next.next;
         size -= 1;
         return removeEle;
     }
 
     public T removeLast() {
-        if (sentinelOrigin.prev == sentinelOrigin) {
-            return null;
+        T removeEle = null;
+        if (size == 0) {
+            return removeEle;
         }
-        T removeEle = (T) sentinelOrigin.prev.item;
-        if (sentinelOrigin.prev.prev != null) {// after remove size > 0
-            sentinelOrigin.prev.prev.next = sentinelOrigin;
-            sentinelOrigin.prev = sentinelOrigin.prev.prev;
-        } else {// after remove size = 0
-            sentinelOrigin.prev = sentinelOrigin;
-            sentinelOrigin.next = sentinelOrigin;
-        }
+        removeEle = (T) sentinelOrigin.prev.item;
+        sentinelOrigin.prev.prev.next = sentinelOrigin;
+        sentinelOrigin.prev = sentinelOrigin.prev.prev;
+        size -= 1;
         return removeEle;
     }
 
     public T get(int index) {
-        if (size == 0) {
-            System.out.println("List size is 0");
-            return null;
-        }
         T item = null;
         Node temp = sentinelOrigin.next;
         try {
-            for (int i=0; i<index; i++) {
+            for (int i=0; i<=index; i++) {
                 item = (T) temp.item;
                 temp = temp.next;
             }
@@ -143,16 +131,19 @@ public class LinkedListDeque<T> {
     }
 
     public T getRecursive(int index) {
+        if (index >= size ) {
+            System.out.println("Index "+ index + " is out of the list index " + (size-1) + ".");
+            return null;
+        }
         return recursiveProcess(index, sentinelOrigin);
     }
 
     private T recursiveProcess(int recDeepth, Node sen) {
         if (recDeepth == 0) {
-            return (T) sen.item;
+            return (T) sen.next.item;
         }
         recDeepth -= 1;
-        recursiveProcess(recDeepth, sen.next);
-        return null;
+        return (T) recursiveProcess(recDeepth, sen.next);
     }
 
 //    public Iterator<T> iterator() {
